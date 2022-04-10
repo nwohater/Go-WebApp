bea971a024e890c974016400dde55ee80ae73b6e
nwohater <bslackey@gmail.com>
2022-04-09 21:15:45 -0700

    folders

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,19 +1,20 @@
-package main
+package handlers
 
 import (
+	"github.com/nwohater/Go-WebApp/pkg/render"
 	"net/http"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.html")
 }
 
 func SuckIt(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "suckit.page.html")
+	render.RenderTemplate(w, "suckit.page.html")
 }
